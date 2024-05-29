96794cf114f4920afa87403b454670b4d126768b
Wonjeong Park <75118947+ParkWonjeong@users.noreply.github.com>
2024-05-29 11:20:08 +0900

    Spring Initializr 생성 프로젝트 업로드

---
+++ src/main/java/com/elice/slowslow/SlowslowApplication.java
@@ -0,0 +1,13 @@
+package com.elice.slowslow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowslowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowslowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/slowslow/SlowslowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.slowslow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowslowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
