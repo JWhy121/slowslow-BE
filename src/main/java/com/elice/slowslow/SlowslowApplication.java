5a2d460491b2baf6909c2eef814675f50573e9ec
[Cloud3] 조한휘 <chh498@naver.com>
2024-06-08 18:51:16 +0900

    Chore : 수정

--- src/main/java/com/elice/slowslow/SlowslowApplication.java
+++ src/main/java/com/elice/slowslow/SlowslowApplication.java
@@ -2,13 +2,9 @@ package com.elice.slowslow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
-@EnableJpaAuditing
+@SpringBootApplication
 public class SlowslowApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(SlowslowApplication.class, args);
