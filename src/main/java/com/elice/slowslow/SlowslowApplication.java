7adb0bc300b8a45e69a1bc396725cacc12bc1bdb
[Cloud3] 조한휘 <chh498@naver.com>
2024-06-08 18:29:40 +0900

    Feat Order 기능 구현

--- src/main/java/com/elice/slowslow/SlowslowApplication.java
+++ src/main/java/com/elice/slowslow/SlowslowApplication.java
@@ -2,9 +2,13 @@ package com.elice.slowslow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@EnableJpaAuditing
 public class SlowslowApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SlowslowApplication.class, args);
